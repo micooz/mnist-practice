68ab4ed1d311885471b8b9de79b3909d5baa1532
micooz <micooz@hotmail.com>
2020-07-28 23:17:11 +0800

    feat: add knn2 which using @tensorflow-models/knn-classifier

---
+++ src/knn2/main.ts
@@ -0,0 +1,44 @@
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import { getTrainData, getTestData } from '../dataset';
+
+const classifier = knnClassifier.create();
+
+const [trainImages, trainLabels] = getTrainData();
+const [testImages, testLabels] = getTestData();
+
+// for (let i = 0; i < trainImages.shape[0]; i++) {
+for (let i = 0; i < 1000; i++) {
+  const trainImage = trainImages.slice(i, 1).flatten();
+  const trainLabel = trainLabels.slice(i, 1).flatten().argMax(0).dataSync()[0];
+
+  classifier.addExample(
+    trainImage,
+    trainLabel,
+  );
+}
+
+async function main() {
+  let acc = '0.000';
+  let correctNum = 0;
+  for (let i = 0; i < testImages.shape[0]; i++) {
+    // for (let i = 0; i < 100; i++) {
+    const testImage = testImages.slice(i, 1).flatten();
+    const testLabel = testLabels.slice(i, 1).flatten().argMax(0).dataSync()[0];
+
+    const result = await classifier.predictClass(
+      testImage,
+      5,
+    );
+
+    const { label, classIndex, confidences } = result;
+    if (testLabel === +label) {
+      correctNum += 1;
+      acc = (correctNum / (i + 1)).toFixed(3);
+    }
+    if ((i + 1) % 100 === 0) {
+      console.log(`total = ${i + 1}  acc = ${acc}`);
+    }
+  }
+}
+
+main();
